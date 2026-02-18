92f4c4f2ebfc6b61e44cbaf63ef038c94df40700
BosshuBots <ajmalitsme@gmail.com>
2026-02-18 14:55:44 +0530

    Update bot.py

--- bothubots-system/bot/bot.py
+++ bothubots-system/bot/bot.py
@@ -1,9 +1,7 @@
 from pyrogram import Client
-from loader import load_core, load_clones
 from config import Config
-import asyncio
+from loader import load_core, load_clones
 
-# Main bot instance
 app = Client(
     "main_bot",
     api_id=Config.API_ID,
@@ -13,9 +11,8 @@ app = Client(
 )
 
 @app.on_start()
-async def start_bot(client):
+async def startup(client):
     await load_core(client)
     await load_clones(client)
 
-if __name__ == "__main__":
-    app.run()
+app.run()
